2171510a821ecf08814d4137c42e03294ba78edd
∅LÏVEÏRÆ <64167003+Oliveirae@users.noreply.github.com>
2021-07-22 00:15:38 -0300

    Update setting.sh

--- linux_config/setting.sh
+++ linux_config/setting.sh
@@ -3,9 +3,6 @@ sudo apt-get update
 
 #Opening the system VNC
 sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils > /dev/null 2>&1
-        
-#Creating User to Login
-sudo useradd -m ${{ github.event.inputs.uname }} && sudo adduser ${{ github.event.inputs.uname }} sudo && echo '${{ github.event.inputs.uname }}:${{ github.event.inputs.pword }}' | sudo chpasswd
 
 # Installing and Configurating Xrdp 
 sudo apt install xrdp && sudo adduser ${{ github.event.inputs.uname }} ssl-cert && sudo systemctl restart xrdp
