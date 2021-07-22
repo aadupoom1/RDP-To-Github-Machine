797750a8dad6c187ae988351e76a673f9c554597
∅LÏVEÏRÆ <64167003+Oliveirae@users.noreply.github.com>
2021-07-21 22:28:46 -0300

    Update setting.sh

--- linux_config/setting.sh
+++ linux_config/setting.sh
@@ -1,5 +1,5 @@
 # Install Desktop Environment
-sudo apt-get update && sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils > /dev/null &
+sudo apt-get update && sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
 
 # Add user
 sudo adduser $1 --gecos "$1,RoomNumber,WorkPhone,HomePhone" --disabled-password
@@ -7,14 +7,14 @@ echo "$1:$2" | sudo chpasswd
 
 
 # Installing and Configurating Xrdp 
-sudo apt install xrdp && sudo adduser $1 ssl-cert && sudo systemctl restart xrdp > /dev/null &
+sudo apt install xrdp && sudo adduser $1 ssl-cert && sudo systemctl restart xrdp
 
 # Download and Install Ngrok
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 ./ngrok authtoken $3
-./ngrok tcp 3389 > /dev/null &
+./ngrok tcp 3389
 
 # Showing you IP for connect
-echo Link for VNC
+echo YOU IP
 curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url'
