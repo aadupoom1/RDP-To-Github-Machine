599b02fe649f19972e77d9a0a0a55fa44cdb6042
∅LÏVEÏRÆ <64167003+Oliveirae@users.noreply.github.com>
2021-07-21 23:58:33 -0300

    Update setting.sh

--- linux_config/setting.sh
+++ linux_config/setting.sh
@@ -1,20 +1,11 @@
-# Install Desktop Environment
-sudo apt-get update && sudo apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
-
-# Add user
-sudo adduser $1 --gecos "$1,RoomNumber,WorkPhone,HomePhone" --disabled-password
-echo "$1:$2" | sudo chpasswd
-
+#Creating User to Login
+sudo useradd -m ${{ github.event.inputs.uname }} && sudo adduser ${{ github.event.inputs.uname }} sudo && echo '${{ github.event.inputs.uname }}:${{ github.event.inputs.pword }}' | sudo chpasswd
 
 # Installing and Configurating Xrdp 
-sudo apt install xrdp && sudo adduser $1 ssl-cert && sudo systemctl restart xrdp
+sudo apt install xrdp && sudo adduser ${{ github.event.inputs.uname }} ssl-cert && sudo systemctl restart xrdp
 
-# Download and Install Ngrok
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
-./ngrok authtoken $3
-./ngrok tcp 3389 > /dev/null &
+#Download and Install Ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip
 
-# Showing you IP for connect
-echo YOU IP
-curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url'
+#Starting Ngrok
+./ngrok authtoken ${{ secrets.NGROK_TOKEN }} && ./ngrok tcp 3389 > /dev/null &
